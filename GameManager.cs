efccef27c3674dd76de4eb40eaa4764cb91a33b4
Lee <winterfive@users.noreply.github.com>
2018-06-28 14:46:06 -0400

    Initial commit

---
+++ GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+    
+    public float timeBetweenShots = 0.15f;
+
+    private float _timer;
+    private bool _isObjectShootable;
+    private RaycastManager _raycastManager;
+    private GameObject _currentFoundObject;
+    private ShapeManager _shapeManager;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        _timer = 0f;
+        _raycastManager = null;
+        _currentFoundObject = null;
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_isObjectShootable && _timer >= timeBetweenShots && Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    //  Destroys object
+    //  void -> void
+    public void Shoot()
+    {
+        _shapeManager.DestroyShape(_currentFoundObject);
+        Debug.Log("Shot an object");
+    }
+
+    // Updates bool isObjectSHootable
+    // void -> void
+    void Shootable()
+    {
+        if (_raycastManager.IfShootable)
+        {
+            _isObjectShootable = true;
+        }
+        else
+        {
+            _isObjectShootable = false;
+        }
+    }
+}
---
+++ ReticleManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+public class ReticleManager : MonoBehaviour {
+
+    public GameObject reticle;
+
+    private RaycastManager _raycastManager;
+    private GameObject _objectFound;
+    private MeshRenderer _meshRenderer;
+    private Vector3 _reticleNormalPosition;
+
+
+	void Awake ()
+    {
+        _raycastManager = null;
+        _objectFound = null;
+        // TODO _reticleNormalPosition = GetComponent camera position and rotation and add to it
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (_raycastManager.HasHitObject)
+        {
+            _objectFound = _raycastManager.GetObjectFound;
+            //PlaceReticle(_objectFound);
+        }
+        else
+        {
+            // place reticle infront camera at default distance
+        }
+	}
+
+    // Places Reticle object on object found by raycast
+    // GameObject -> void
+    void PlaceReticle()
+    {
+        Debug.Log("Reticle thinks this is objectFound: " + _objectFound);
+    }
+
+    void ChangeReticleColor()
+    {
+        
+        //Debug.Log("Reticle thinks this is objectFound: " + _objectFound);
+
+        // TODO Display reticle on foundObject (position, rotation)
+
+        // TODO if(objectFound is Shootable), change color of reticle
+    }
+
+}
