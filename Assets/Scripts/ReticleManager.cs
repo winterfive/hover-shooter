c1603e8e67bce77d4ea4be1df626ecaa4947d7f9
winterfive <piealacode@gmail.com>
2018-07-10 16:03:11 -0400

    Added event methods and set fields to private where applicable

--- Assets/Scripts/ReticleManager.cs
+++ Assets/Scripts/ReticleManager.cs
@@ -4,43 +4,33 @@ using UnityEngine.UI;
 
 public class ReticleManager : MonoBehaviour {
 
-    [SerializeField] private float _DefaultDistance = 5f;      // The default distance away from the camera the reticle is placed.
-    [SerializeField] private bool _UseNormal;                  // Whether the reticle should be placed parallel to a surface.
-    [SerializeField] private Image _Image;                     // Reference to the image component that represents the reticle.
-    [SerializeField] private Transform _ReticleTransform;      // We need to affect the reticle's transform.
-    [SerializeField] private Transform _Camera;                // The reticle is always placed relative to the camera.
+    
+    
+    public Transform _camera;
+    public float _defaultDistance = 2f;      // The default distance away from the camera the reticle is placed.
+    public GameObject reticle;
 
-    private Vector3 _OriginalScale;                            // Since the scale of the reticle changes, the original scale needs to be stored.
-    private Quaternion _OriginalRotation;                      // Used to store the original rotation of the reticle.
+    private bool _useNormal;                  // Whether the reticle should be placed parallel to a surface.    
+    private Transform _reticleTransform;
+    private Vector3 _originalScale;           // Since the scale of the reticle changes, the original scale needs to be stored.
+    private Quaternion _originalRotation;     // Used to store the original rotation of the reticle.
 
 
     public bool UseNormal
     {
-        get { return _UseNormal; }
-        set { _UseNormal = value; }
+        get { return _useNormal; }
+        set { _useNormal = value; }
     }
 
 
-    public Transform ReticleTransform { get { return _ReticleTransform; } }
+    public Transform ReticleTransform { get { return reticle.transform; } }
 
 
     private void Awake()
     {
-        // Store the original scale and rotation.
-        _OriginalScale = _ReticleTransform.localScale;
-        _OriginalRotation = _ReticleTransform.localRotation;
-    }
-
-
-    public void Hide()
-    {
-        _Image.enabled = false;
-    }
-
-
-    public void Show()
-    {
-        _Image.enabled = true;
+        _reticleTransform = reticle.GetComponent<Transform>();
+        _originalScale = reticle.transform.localScale;
+        _originalRotation = reticle.transform.localRotation;
     }
 
 
@@ -48,28 +38,38 @@ public class ReticleManager : MonoBehaviour {
     public void SetPosition()
     {
         // Set the position of the reticle to the default distance in front of the camera.
-        _ReticleTransform.position = _Camera.position + _Camera.forward * _DefaultDistance;
+        _reticleTransform.position = _camera.position + _camera.forward * _defaultDistance;
 
         // Set the scale based on the original and the distance from the camera.
-        _ReticleTransform.localScale = _OriginalScale * _DefaultDistance;
+        _reticleTransform.localScale = _originalScale * _defaultDistance;
 
         // The rotation should just be the default.
-        _ReticleTransform.localRotation = _OriginalRotation;
+        _reticleTransform.localRotation = _originalRotation;
     }
 
 
     // This overload of SetPosition is used when the VREyeRaycaster has hit something.
     public void SetPosition(RaycastHit hit)
     {
-        _ReticleTransform.position = hit.point;
-        _ReticleTransform.localScale = _OriginalScale * hit.distance;
+        _reticleTransform.position = hit.point;
+        _reticleTransform.localScale = _originalScale * hit.distance;
 
         // If the reticle should use the normal of what has been hit...
-        if (_UseNormal)
+        if (_useNormal)
             // ... set it's rotation based on it's forward vector facing along the normal.
-            _ReticleTransform.rotation = Quaternion.FromToRotation(Vector3.forward, hit.normal);
+            _reticleTransform.rotation = Quaternion.FromToRotation(Vector3.forward, hit.normal);
         else
             // However if it isn't using the normal then it's local rotation should be as it was originally.
-            _ReticleTransform.localRotation = _OriginalRotation;
+            _reticleTransform.localRotation = _originalRotation;
+    }
+
+    private void OnEnable()
+    {
+        RaycastManager.OnNewObjectFound += SetPosition;
+    }
+
+    private void OnDisable()
+    {
+        RaycastManager.OnNewObjectFound -= SetPosition;
     }
 }
