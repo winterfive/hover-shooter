4fb5feb92b4104c61dbca4f32d1d66a69d42b29e
winterfive <piealacode@gmail.com>
2018-07-12 16:05:35 -0400

    Added UpdateScore(), AddToScore(), and fields

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,22 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public static int score;
+    public Text scoreText;
 
-    Text text;
+    int _shapePointValue = 10;    
 
 
-    void Awake()
+    void Start()
     {
-        text = GetComponent<Text>();
+        //scoreText = GetComponent<Text>();
         score = 0;
+        UpdateScore();
     }
 
 
     void Update()
     {
-        text.text = "Score: " + score;
+        UpdateScore();
+    }
+
+
+    public void AddToScore()
+    {
+        score += _shapePointValue;
+    }
+
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
     }
 }
