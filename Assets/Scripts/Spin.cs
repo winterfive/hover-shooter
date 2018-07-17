15082e6368a49b7504e72cdbce2bcc800a024524
winterfive <piealacode@gmail.com>
2018-07-17 13:35:26 -0400

    Created Spin script for spinning gameObjects

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+    public float speed;
+
+
+    private void FixedUpdate()
+    {
+        gameObject.transform.Rotate(0f, speed, 0f, Space.Self);
+    }
+}
