88922fb6c573dfe2d5a744dc4856f7ce401c5df1
winterfive <piealacode@gmail.com>
2018-07-20 15:26:33 -0400

    Created DroneMover script

---
+++ Assets/Scripts/DroneMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DroneMover : MonoBehaviour {
+
+    private Vector3 camPosition;
+    
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        camPosition = Camera.main.gameObject.transform.position;
+        agent.destination = camPosition;
+    }
+}
