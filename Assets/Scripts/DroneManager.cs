0dbfabbe10595391096ebe5ff869847ac4bde6e1
winterfive <piealacode@gmail.com>
2018-07-21 11:51:58 -0400

    Changed name of DroneMover to DroneManager

---
+++ Assets/Scripts/DroneManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//  Drone class can handle more then one model of drone
+//  Each model will ahve it's own animations, colors, and effects
+
+public class DroneManager : MonoBehaviour {
+
+    public Transform[] Spawnpoints;
+    public float spawnTime;
+    public float waitToSpawn;
+
+    private Vector3 camPosition;    
+    
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        camPosition = Camera.main.gameObject.transform.position;
+        agent.destination = camPosition;
+    }
+
+    void SpawnDrones()
+    {
+        // Get random spawnpoint value
+        // Get drone from object pool
+        // Set shooting to true
+        // Get random height from range to spawn at
+        // "Spawn" drone
+    }
+
+    void DestroyDrone()
+    {
+        // listens for event
+        // stop color change in EffectManager
+        // Call explosion script in EffectsManager
+        // stop shooting script, shooting = false
+        // begin drone failling anim (drone crashes, tiled on floor)
+        // Call smoke script in EffectsManager
+        // Drone falls through floor slowly
+        // Requeue drone object
+    }
+
+    void DroneShoot()
+    {
+        // If(drone can see player)
+        // {  
+        //      Shoot()  Use raycastManger to make a line?
+        // }
+    }
+
+    void LookAtPlayer()
+    {
+
+    }
+}
