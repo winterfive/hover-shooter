48a52cc60476e721840ca4e7a84c83df34d5d6ce
winterfive <piealacode@gmail.com>
2018-07-27 16:03:09 -0400

    Changed DroneMOver name to DroneActions

---
+++ Assets/Scripts/DroneActions.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class DroneActions : MonoBehaviour {
+
+    public float altitudeMin, altitudeMax;
+    public float xMin, xMax, zMin, zMax, glowSpeed;
+    public Color firstGlow, secondGlow;
+    
+    private Transform _glowTransform;
+    private Renderer _glowRend;
+    private NavMeshAgent _agent;
+    private Transform _camTransform;
+    private float _timeAtSpawn;
+
+
+    void Start ()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _camTransform = Camera.main.gameObject.transform;
+        _agent.baseOffset = Random.Range(altitudeMin, altitudeMax);        
+
+        _glowTransform = FindChildWithGlow();
+        _glowRend = _glowTransform.GetComponent<Renderer>();
+
+        GotoRandomPoint();
+
+        InvokeRepeating("LerpColor", 0f, 0.1f);
+    }
+
+
+    private void Update()
+    {
+        if (!_agent.pathPending && _agent.remainingDistance < 0.5f)
+        {
+            GotoPlayer();
+        }
+    }
+
+
+    /*
+     * Changes drone destination to camera
+     * void -> void
+     */
+    private void GotoPlayer()
+    {
+        _agent.destination = _camTransform.position;
+    }
+
+    /*
+     * Drone turret always pointed at player
+     * void -> void
+     */
+     void LookAtPlayer()
+    {
+
+    }
+
+
+    /*
+     * Assigns and directs drone to random point
+     * void -> void
+     */
+    void GotoRandomPoint()
+    {
+        Vector3 newVector = CreateRandomPosition();
+        _agent.destination = newVector;        
+    }
+
+
+    /*
+     * Creates Vector3 w/ random values w/in range
+     * void -> Vector3
+     */
+    private Vector3 CreateRandomPosition()
+    {
+        Vector3 randomPosition;
+
+        randomPosition.x = Random.Range(xMin, xMax);
+        randomPosition.y = _agent.baseOffset;
+        randomPosition.z = Random.Range(zMin, zMax);
+
+        return randomPosition;        
+    }
+
+
+    void LerpColor()
+    {
+        float pingpong = Mathf.PingPong(Time.time * glowSpeed, 1.0f);
+
+        _glowRend.material.color = Color.Lerp(firstGlow, secondGlow, pingpong);
+    }
+
+    /*
+     * Finds child object with "Glow" tag
+     * void -> transform
+     */
+    private Transform FindChildWithGlow()
+    {
+        Transform firstChild = this.transform.Find("Body");
+        Transform[] components = firstChild.GetComponentsInChildren<Transform>();
+            
+        foreach(Transform t in components)
+        {
+            if(t.gameObject.CompareTag("Glow"))
+            {
+                return t;
+            }
+        }
+
+        return null;
+    }
+}
