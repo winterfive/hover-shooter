33d0135214491691e7edd5eabeeb7699a560ce14
winterfive <piealacode@gmail.com>
2018-07-30 10:49:52 -0400

    Added RevertReticleColor() and CheckOBjectFound(), altered ChangeReticleColor()

--- Assets/Scripts/ReticleManager.cs
+++ Assets/Scripts/ReticleManager.cs
@@ -8,12 +8,14 @@ public class ReticleManager : MonoBehaviour {
     public float _defaultDistance = 2f;
     public GameObject reticle;
     public RaycastManager raycastManager;
+    public Color targetAquiredColor, noTargetColor;
 
     private Transform _reticleTransform;
     private Vector3 _originalScale;
     private Quaternion _originalRotation;
+    private Color _reticleColor;
     private RaycastHit _currentHit;
-
+    private GameObject _currentObject;
 
     public Transform ReticleTransform { get { return reticle.transform; } }
 
@@ -23,32 +25,78 @@ public class ReticleManager : MonoBehaviour {
         _reticleTransform = reticle.GetComponent<Transform>();
         _originalScale = reticle.transform.localScale;
         _originalRotation = reticle.transform.localRotation;
+        _reticleColor = reticle.GetComponent<Renderer>().material.color;
     }
 
 
-    // Used when RaycastManager hasn't hit anything.
-    // void -> void
+    /*
+     * Sets reticle to default position and scale
+     * void -> void
+     */
     public void SetPosition()
     {
         _reticleTransform.position = _camera.position + _camera.forward * _defaultDistance;
-        // Make the reticle larger at long distance so we can see it better
         _reticleTransform.localScale = _originalScale * _defaultDistance;
         _reticleTransform.localRotation = _originalRotation;
+        _reticleColor = noTargetColor;
     }
 
 
-    // Used when the RaycastManager has hit something.
-    // RaycastHit -> void
+    /*
+     * Sets reticle to indicate object found
+     * RaycastHit -> void
+     */
     public void SetPosition(RaycastHit hit)
     {
         _reticleTransform.position = hit.point;
         _reticleTransform.localScale = _originalScale * hit.distance;
-        _reticleTransform.rotation = Quaternion.FromToRotation(Vector3.forward, hit.normal);        
+        _reticleTransform.rotation = Quaternion.FromToRotation(Vector3.forward, hit.normal);
+    }
+
+
+    /*
+     * Changes color of reticle if object found is enemy 
+     * gameObject -> void
+     */
+    public void ChangeReticleColor(GameObject go)
+    {
+        _reticleColor = targetAquiredColor;
+    }
+
+
+    /*
+     * Sets color of reticle if object is not enemy 
+     * gameObject -> void
+     */
+    public void RevertReticleColor(GameObject go)
+    {
+        _reticleColor = noTargetColor;
+    }
+
+
+    /*
+     * Checks if current object seen by reticle is an enemy
+     * void -> void
+     */
+    public void CheckObjectFound()
+    {
+        _currentObject = raycastManager.GetCurrentFoundObject();
+
+        if(_currentObject.tag == "Drone")
+        {
+            ChangeReticleColor(_currentObject);
+        }
+        else
+        {
+            RevertReticleColor(_currentObject);
+        }
     }
 
     
-    // Calls SetPosition
-    // void -> void
+   /*
+    * Listens for RaycastManager updates
+    * void -> void
+    */ 
     public void CheckNormalFound()
     {
         _currentHit = raycastManager.GetCurrentHit();
@@ -59,10 +107,12 @@ public class ReticleManager : MonoBehaviour {
     private void OnEnable()
     {
         RaycastManager.OnNewNormalFound += CheckNormalFound;
+        RaycastManager.OnNewObjectFound += CheckObjectFound;
     }
 
     private void OnDisable()
     {
         RaycastManager.OnNewNormalFound -= CheckNormalFound;
+        RaycastManager.OnNewObjectFound -= CheckObjectFound;
     }
 }
