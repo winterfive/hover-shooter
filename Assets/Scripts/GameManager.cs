cc6981733900d8f5802bdf6b5fc94ed46a63260d
winterfive <piealacode@gmail.com>
2018-08-03 12:49:05 -0400

    DestroyDrone() added to Update()

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+    
+    public float timeBetweenShots = 0.15f;
+    public DroneManager droneManager;
+
+    private float _timer;    
+
+
+    void Awake()
+    {
+        _timer = 0f;
+    }
+
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && _timer >= timeBetweenShots && Time.timeScale != 0)
+        {
+            droneManager.DestroyDrone();
+        }
+    }
+}
