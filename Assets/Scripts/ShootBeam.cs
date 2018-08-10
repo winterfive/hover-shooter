9e1d0690a3d6cf02a075431f5b88378f7814e4e2
winterfive <piealacode@gmail.com>
2018-08-10 14:37:44 -0400

    Created ShootBeam script to enable an ddisable line renderer on GunTip object

---
+++ Assets/Scripts/ShootBeam.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBeam : MonoBehaviour {
+
+    public int droneShotRange;
+
+    private RaycastHit _hit;
+    private LineRenderer _line;
+
+    private void Start()
+    {
+        _line = this.GetComponent<LineRenderer>();
+        _line.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (Time.frameCount % 2 == 0)
+        {
+            if (Physics.Raycast(this.transform.position, this.transform.forward, out _hit, droneShotRange))
+            {
+                if (_hit.transform.gameObject.tag == "MainCamera")
+                {
+                    _line.SetPosition(0, this.transform.position);
+                    _line.SetPosition(1, _hit.point);
+                    _line.enabled = false;
+
+                    // Call event to notify UI (score) and gameManger or playermanager (player health)
+                }
+            }
+        }
+    }
+}
