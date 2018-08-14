691352ad1e71393d3a5dfc4dd2d32d36b4abb7d5
winterfive <piealacode@gmail.com>
2018-08-14 13:38:20 -0400

    Created DroneShooting class

--- Assets/Scripts/DroneShooting.cs
+++ Assets/Scripts/DroneShooting.cs
@@ -7,32 +7,44 @@ public class DroneShooting : MonoBehaviour {
     public GameObject missle;
     public PoolManager poolManager;
     public int poolSize;
+    public float missleSpeed;
 
     private Vector3 _camPosition;
     private Transform _gunTipTransform;
     private List<GameObject> _missles;
     private RaycastHit _hit;
 
+
     private void Start()
     {
-        _camPosition = Camera.main.gameObject.transform.position;
-        _gunTipTransform = this.gameObject.transform;
+        _camPosition = Camera.main.gameObject.transform.position;        
         _missles = poolManager.CreateList(missle, poolSize);
     }
 
-    private void Update()
+
+    public void ShootAtPlayer()
     {
-        if (Physics.Raycast(_gunTipTransform.position, _gunTipTransform.forward, out _hit, 125))
+        GameObject readyMissle = GetObjectFromPool();
+        Rigidbody rb = readyMissle.GetComponent<Rigidbody>();
+
+        readyMissle.transform.position = _gunTipTransform.position;
+        rb.velocity = transform.forward * missleSpeed;
+
+        Debug.Log("ShootAtPlayer has been called");
+    }
+
+
+    GameObject GetObjectFromPool()
+    {
+        foreach (GameObject missle in _missles)
         {
-            if (_hit.transform.tag == "Player")
+            if (!missle.activeInHierarchy)
             {
-                ShootAtPlayer();
+                return missle;
             }
         }
+        return null;
     }
 
-    private void ShootAtPlayer()
-    {
-
-    }
+    // TODO Listens for DroneActions ShootAtPlayer call
 }
