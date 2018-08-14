f7da37b7dd2dd8eea680eb7176e1c9c5ae9be705
winterfive <piealacode@gmail.com>
2018-08-14 12:34:31 -0400

    Created Enemy SHooting Manager script

---
+++ Assets/Scripts/EnemyShootManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemyShootManager : MonoBehaviour {
+
+    private Vector3 _camPosition;
+    private Transform _gunTipTransform;
+    private RaycastHit _hit;
+
+    private void Start()
+    {
+        _camPosition = Camera.main.gameObject.transform.position;
+        _gunTipTransform = this.gameObject.transform;
+    }
+
+    private void Update()
+    {
+        if (Physics.Raycast(_gunTipTransform.position, _gunTipTransform.forward, out _hit, 125))
+        {
+            if (_hit.transform.tag == "Player")
+            {
+                ShootAtPlayer();
+            }
+        }
+    }
+
+    private void ShootAtPlayer()
+    {
+
+    }
+}
