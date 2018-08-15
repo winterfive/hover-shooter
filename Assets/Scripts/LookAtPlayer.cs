5f84c25d631f0115f2c4d84f38e29d9d0f87af2d
winterfive <piealacode@gmail.com>
2018-08-15 15:41:06 -0400

    Created LookAtPlayer script

---
+++ Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour {
+
+    private Transform _camTransform;
+
+    
+    void Start ()
+    {
+        _camTransform = Camera.main.gameObject.transform;
+    }
+
+	
+	void Update ()
+    {
+        PointTurretAtPlayer();		
+	}
+
+
+    /*
+     * Rotates turret towards player on Y axis
+     * void -> void
+     */
+    private void PointTurretAtPlayer()
+    {
+        if (this.isActiveAndEnabled)
+        {
+            Vector3 newVector = new Vector3(this.transform.position.x - _camTransform.position.x,
+                                            0f,
+                                            this.transform.position.z - _camTransform.position.z);
+
+            this.transform.rotation = Quaternion.LookRotation(newVector);
+        }
+    }
+}
