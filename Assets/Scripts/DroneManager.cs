9a55df87ee3167f8b1783da0b3c9c1168efc55aa
winterfive <piealacode@gmail.com>
2018-08-18 11:40:36 -0400

    Removed public variable to raycast Manager

--- Assets/Scripts/DroneManager.cs
+++ Assets/Scripts/DroneManager.cs
@@ -10,14 +10,12 @@ public class DroneManager : GenericManager<DroneManager>
     public GameObject dronePrefab;
     public int dronePoolSize;
     public float xMin, xMax, yMin, yMax, zMin, zMax;
-    public float missleSpeed;
     
     [SerializeField] private float _timeBetweenSpawns;
     [SerializeField] private float _waitToSpawn;
 
     private Transform _camTransform;
     private List<GameObject> _drones;
-    private List<GameObject> _missles;
     private PoolManager _poolManager;
     
 
@@ -25,19 +23,12 @@ public class DroneManager : GenericManager<DroneManager>
     {
         _poolManager = PoolManager.Instance;
         _drones = _poolManager.CreateList(dronePrefab, dronePoolSize);
-        _missles = _poolManager.CreateList(misslePrefab, misslePoolSize);
     }
 
     void Start()
     {
         _camTransform = Camera.main.gameObject.transform;
         InvokeRepeating("SpawnDrone", _waitToSpawn, _timeBetweenSpawns);
-    }
-
-
-    private void Update()
-    {
-        // TODO if missle reaches player collider, set active to false
     }
 
 
@@ -51,7 +42,7 @@ public class DroneManager : GenericManager<DroneManager>
 
         spawnPointIndex = Random.Range(0, spawnpoints.Length);
 
-        GameObject drone = GetObjectFromPool(_drones);
+        GameObject drone = _poolManager.GetObjectFromPool(_drones);
 
         if (drone)
         {
@@ -63,25 +54,6 @@ public class DroneManager : GenericManager<DroneManager>
         {
             Debug.Log("No more drones in list to use.");
         }               
-    }
-
-
-    private GameObject GetObjectFromPool(List<GameObject> l)
-    {
-        foreach (GameObject g in l)
-        {
-            if (!g.activeInHierarchy)
-            {
-                return g;
-            }            
-        }
-        return null;
-    }
-
-
-    public void ReturnToPool(GameObject go)
-    {
-        go.SetActive(false);
     }
 
 
