017e89ccf040f406d3215e339c943973487545a4
winterfive <piealacode@gmail.com>
2018-08-24 16:48:44 -0400

    Created UIManager class

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : GenericManager<UIManager> {
+
+    // Listens for change in score
+
+    // Listens for change in player health
+
+    private void OnEnable()
+    {
+        
+    }
+
+
+    private void OnDisable()
+    {
+        
+    }
+}
