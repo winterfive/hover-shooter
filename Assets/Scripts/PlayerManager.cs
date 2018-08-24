c5d46b39db5e9e8e47e6a9ed46c155c9b8d28d45
winterfive <piealacode@gmail.com>
2018-08-24 16:33:26 -0400

    Created PlayerManager script

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public int playerScore;
+    public int playerHealth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void UpdateScore()
+    {
+        // Listens to ProjectileManager for missleHit
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
