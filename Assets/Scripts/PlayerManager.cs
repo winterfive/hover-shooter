b10cc82b15a6346eb6041bded771f79e8e251a4e
winterfive <piealacode@gmail.com>
2018-08-24 16:38:26 -0400

    Added pseudocode to UpdateHealth() and UpdateScore()

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,20 +6,20 @@ public class PlayerManager : MonoBehaviour {
 
     public int playerScore;
     public int playerHealth;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
 
     private void UpdateScore()
     {
+        // Listens to ProjectileActions for missleHit
+        // Depending on type of projectile has hit, adjust score value
+        // Broadcast change in score value
+    }
+
+    private void UpdateHealth()
+    {
         // Listens to ProjectileManager for missleHit
+        // Updates health depending on type of projectile that has hit player
+        // Broadcast change in health value
     }
 
     private void OnEnable()
