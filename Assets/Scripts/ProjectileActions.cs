60b21830bbe3cdb4f9dab5891b46ddbfb5a71ee6
winterfive <piealacode@gmail.com>
2018-08-28 17:33:23 -0400

    Added OnCollisionEnter() with calls to event methods (playerhit, shieldhit)

--- Assets/Scripts/ProjectileActions.cs
+++ Assets/Scripts/ProjectileActions.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 public class ProjectileActions : MonoBehaviour
 {
     public int missleSpeed;
+    public delegate void PlayerHit();
+    public static PlayerHit OnPlayerHit;
+    public delegate void ShieldHit();
+    public static ShieldHit OnShieldHit;
     
     private GameObject _missle;
     private Transform _camTransform;
@@ -17,14 +21,42 @@ public class ProjectileActions : MonoBehaviour
         _missle = this.gameObject;
     }
 
-    void Update()
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (OnPlayerHit != null)
+            {
+                OnPlayerHit();
+            }
+        }
+
+        if (col.gameObject.tag == "Shield")
+        {
+            if (OnShieldHit != null)
+            {
+                OnShieldHit();
+            }
+        }
+
+        _missle.SetActive(false);
+    }
+
+
+    void FixedUpdate()
     {
         if (_missle.activeInHierarchy)
         {
             MissleFly();
-        }
+        }       
     }
 
+
+    /*
+     * Checks if missle distance to see if it should continue flying
+     * void -> void
+     */
     private void MissleFly()
     {
         if (Vector3.Distance(_missle.transform.position, _camTransform.position) > 0.5)
@@ -32,21 +64,6 @@ public class ProjectileActions : MonoBehaviour
             float step = missleSpeed * Time.deltaTime;
             _missle.transform.position = Vector3.MoveTowards(_missle.transform.position, _camTransform.position, step);
             _missle.transform.LookAt(_camTransform);
-        }
-        else
-        {
-            //Missle has hit something
-            //Check what missle hit
-            //if (missle hit player)
-            //{
-            //    BroadcastMessage OnPlayerHit;
-            //}
-            //else
-            //{
-            //    BroadcastMessage OnShieldHit;
-            //}
-
-            _missle.SetActive(false);
         }
     }
 }
