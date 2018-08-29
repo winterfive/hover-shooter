14a4b649d2be217d91ebbb1e0a67d05c6b0d58e3
winterfive <piealacode@gmail.com>
2018-08-29 15:52:38 -0400

    Created script for player's gun tip line rendereres (laser beams)

---
+++ Assets/Scripts/PlayerLasers.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerLasers : MonoBehaviour {
+
+    /*
+     * Script placed on empty gameObjects at the tips of
+     * the player's guns to enable laser fire
+     */
+
+    public ReticleManager reticleManager;
+
+    private LineRenderer _line;
+    private GameObject _gunTip;
+    private Transform _reticle;
+
+
+    private void Start()
+    {
+        _gunTip = this.gameObject;
+        _line = _gunTip.GetComponent<LineRenderer>();
+        _line.enabled = false;        
+    }
+
+    public void ShootLasers()
+    {
+        StopCoroutine("FireLasers");
+        StartCoroutine("FireLasers");
+    }
+
+    private IEnumerator FireLasers()
+    {
+        _reticle = reticleManager.GetReticleTransform();
+
+        if (_reticle)
+        {
+            _line.enabled = true;
+
+            Ray ray = new Ray(transform.position, transform.forward);
+
+            _line.SetPosition(0, ray.origin);
+            _line.SetPosition(1, _reticle.position);
+
+            _line.enabled = false;
+            yield return null;
+        }
+        else
+        {
+            yield return null;
+        }        
+    }
+
+    private void OnEnable()
+    {
+        GameManager.OnShoot += ShootLasers;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnShoot -= ShootLasers;
+    }
+}
