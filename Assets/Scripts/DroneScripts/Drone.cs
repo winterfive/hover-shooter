57ab4773cbf4482c764f22f7e837e08a3a032cdb
winterfive <piealacode@gmail.com>
2018-09-17 18:05:31 -0400

    Removed () bodies from all ()s

--- Assets/Scripts/DroneScripts/Drone.cs
+++ Assets/Scripts/DroneScripts/Drone.cs
@@ -4,64 +4,49 @@ using UnityEngine;
 
 public class Drone : MonoBehaviour
 {
+    public int minSpeed, maxSpeed;
+    public float xMin, xMax, yMin, yMax, zMin, zMax;
+    public Transform[] Spawnpoints;
+    public Transform[] EndPoints;
+    public float glowSpeed;
+
     private bool _hasBeenHit;
     private Transform _camTransform = Camera.main.transform;
+    private Color _defaultGlow;
+
 
 
     /*
      * Spawns gameObject at random spawnpoint
      * void -> void
      */
-    void SpawnDrone()
-    {
-        
-    }
+    void SpawnDrone() { }
 
 
     /*
      * Drone moves to position
      * void -> void
      */
-     void Move()
-    {
-        
-    }
+    void Move() { }
 
 
     /*
      * Pingpongs drone glow steadily from one color to another
      * void -> void
      */
-    private void LerpColor()
-    {
-        
-    }
+    private void LerpColor() { }
 
 
     /*
      * Creates Vector3 w/ random values for x & z w/in range
      * void -> Vector3
      */
-    public Vector3 CreateRandomPosition()
-    {
-        Vector3 randomPosition;
-
-        randomPosition.x = Random.Range(xMin, xMax);
-        randomPosition.y = Random.Range(yMin, yMax);
-        randomPosition.z = Random.Range(zMin, zMax);
-
-        return randomPosition;
-    }
+    public void CreateRandomPosition() { }
 
 
     /*
      * Returns a random endPoint
      * void -> Vector3
      */
-    public Vector3 SelectLastPosition()
-    {
-        int index = Random.Range(0, endPoints.Length);
-        Vector3 endPosition = endPoints[index].position;
-        return endPosition;
-    }
+    public void SelectLastPosition() { }
 }
