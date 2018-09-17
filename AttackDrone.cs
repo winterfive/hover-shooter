169771b1eae73b53cfd789cf2dee3adddfd409e5
winterfive <piealacode@gmail.com>
2018-09-17 14:55:58 -0400

    Refactoring code, initial commit

---
+++ AttackDrone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEditor;
+
+public class AttackDrone : Drone, MonoBehaviour
+{
+    private _attackDroneManagerReference;
+
+    void Awake()
+    {
+        _poolManager = Drone.PoolManager.Instance;
+        _drones = _poolManager.CreateList(dronePrefab, dronePoolSize);
+    }
+
+    void Start()
+    {
+        GameObject attackDroneManagerObject = GameObject.FindWithTag("ScriptManager");
+        if (attackDroneManagerObject != null)
+        {
+            _attackDroneManagerReference = attackDroneManagerObject.GetComponent<DroneManager>();
+        }
+
+        if (_droneManagerReference == null)
+        {
+            Debug.Log("Cannot find DroneManager script");
+        }
+    }
+}
