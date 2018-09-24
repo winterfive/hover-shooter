89e31899f89cae9ce305aaec228b674d8d289f74
winterfive <piealacode@gmail.com>
2018-09-24 14:45:45 -0400

    created PlayerManager class

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : GenericManager<PlayerManager>
+{ 
+    private bool isAlive;
+
+    public bool IsAlive() { return isAlive; }
+
+    private void Awake()
+    {
+        // value change so that droen spawning isn't a runon
+        while (Time.time < 30f)
+        {
+            isAlive = true;
+        }        
+    }
+
+    // TODO player health > 0, true, else isAlive is false
+}
