e0b85d57cd5afc57745c41ce2923e44668c16ff1
winterfive <piealacode@gmail.com>
2018-09-25 20:08:33 -0400

    Added LerpColor(), FindChildWithTag(), and LookAt()

--- Assets/Scripts/DroneScripts/AttackDrone.cs
+++ Assets/Scripts/DroneScripts/AttackDrone.cs
@@ -1,23 +1,25 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 public class AttackDrone : MonoBehaviour
 {
-    private Transform _thisTransform;
     private Vector3 _camPosition;
     private NavMeshAgent _agent;
     private AttackDroneManager _attackDroneManagerReference;
     private bool _movingToMidpoint;
-    private Transform turretTransform;
+    private Transform _turretTransform;
+    private Renderer _glowRenderer;
+    private Color _defaultGlowColor;
 
 
     void Awake()
     {
         _camPosition = Camera.main.transform.position;
-        _thisTransform = this.gameObject.transform;
         _agent = this.gameObject.GetComponent<NavMeshAgent>();
         _movingToMidpoint = false;
+        _turretTransform = FindChildWithTag("Turret");
+        _glowRenderer = FindChildWithTag("Glow").GetComponent<Renderer>();
+        _defaultGlowColor = _glowRenderer.material.color;
     }
 
 
@@ -30,15 +32,14 @@ public class AttackDrone : MonoBehaviour
 
             if (_agent.isOnNavMesh && _agent.isActiveAndEnabled)
             {
-                turretTransform = _attackDroneManagerReference.FindChildWithTag("turret").transform;
-                SetRandomDestination();
-                _agent.speed = Random.Range(_attackDroneManagerReference.minSpeed, _attackDroneManagerReference.maxSpeed);
-                _agent.baseOffset = Random.Range(_attackDroneManagerReference.altitudeMin, _attackDroneManagerReference.altitudeMax);
+                SetRandomDestination(); //TODO Move this method to parent
+                _agent.speed = Random.Range(_attackDroneManagerReference.minSpeed, _attackDroneManagerReference.maxSpeed);  //TODO make a method for this in parent
+                _agent.baseOffset = Random.Range(_attackDroneManagerReference.altitudeMin, _attackDroneManagerReference.altitudeMax);   //TODO "
                 _movingToMidpoint = true;
             }
             else
             {
-                _agent.gameObject.SetActive(false);
+                // TODO using event, send this to the poolmanager for setactive(false)
                 Debug.Log("Attack drone returned to pool (not on navMesh)");
             }
         }
@@ -53,6 +54,7 @@ public class AttackDrone : MonoBehaviour
     void Update()
     {
         LookAt(_camPosition);
+        LerpColor();
 
         // Check if drone is close to mid point or end point
         if (Time.frameCount % 10 == 0)
@@ -66,7 +68,7 @@ public class AttackDrone : MonoBehaviour
                 }
                 else
                 {
-                    _thisTransform.gameObject.SetActive(false);
+                    this.gameObject.SetActive(false);
                 }
             }
         }
@@ -79,16 +81,18 @@ public class AttackDrone : MonoBehaviour
      */
     public void LookAt(Vector3 target)
     {
-        Vector3 newVector = new Vector3(turretTransform.position.x - target.x,
+        Vector3 newVector = new Vector3(_turretTransform.position.x - target.x,
                                         0f,
-                                        turretTransform.position.z - target.z);
+                                        _turretTransform.position.z - target.z);
 
-        turretTransform.rotation = Quaternion.LookRotation(v);
+        _turretTransform.rotation = Quaternion.LookRotation(newVector);
     }
 
 
     private void SetRandomDestination()
     {
+        // TODO Move the guts of this to parent, ahve this method call the method in parent
+        // TODO Maybe move to DroneManager
         Vector3 point = _attackDroneManagerReference.CreateRandomDestination();
         point.y = _agent.baseOffset;
         _agent.destination = point;
@@ -97,8 +101,43 @@ public class AttackDrone : MonoBehaviour
 
     private void GoToEndPoint()
     {
+        // TODO Move the guts of this to parent, have this method call the method in parent
+        // TODO Maybe move to DroneManager
         Vector3 endPoint = _attackDroneManagerReference.SetEndPoint();
         endPoint.y = _agent.baseOffset;
         _agent.destination = endPoint;
+    }
+
+
+    /*
+     * Finds child transform with tag
+     * void -> transform
+     */
+    public Transform FindChildWithTag(string a)
+    {
+        Transform[] components = this.GetComponentsInChildren<Transform>();
+
+        foreach (Transform t in components)
+        {
+            if (t.gameObject.CompareTag(a))
+            {
+                return t;
+            }
+        }
+        return null;
+    }
+
+
+    /*
+     * Pingpongs color steadily from one color to another
+     * void -> void
+     */
+    private void LerpColor()
+    {
+        if (_glowRenderer)
+        {
+            float pingpong = Mathf.PingPong(Time.time * _attackDroneManagerReference.glowSpeed, 1.0f);
+            _glowRenderer.material.color = Color.Lerp(_defaultGlowColor, _attackDroneManagerReference.secondGlow, pingpong);
+        }
     }
 }
