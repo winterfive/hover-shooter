32872722cfb50c2aefc24a75978ef6865a4e8fc3
winterfive <piealacode@gmail.com>
2018-09-27 16:28:03 -0400

    Created class

---
+++ Assets/Scripts/EffectsManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsManager : GenericManager<EffectsManager> {
+
+    public Color[] destructionColors;
+    public float waitBetweenColors;
+
+    private GameObject _shotObject;
+    private RaycastManager _raycastManager;
+
+    
+    void Awake ()
+    {
+        _raycastManager = RaycastManager.Instance;
+	}
+
+
+    public void DissolveEnemy()
+    {
+
+    }
+
+
+    private IEnumerator FadeEffect()
+    {
+        // TODO This required turning on transparent in the material which is not good for mobile :(
+        Renderer[] components = _shotObject.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer r in components)
+        {
+            r.material.mainTexture = null;
+        }
+
+        foreach (Color c in destructionColors)
+        {
+            foreach (Renderer r in components)
+            {
+                r.material.color = c;
+                Debug.Log("Fading out drone to color: " + c);
+            }
+
+            yield return new WaitForSeconds(waitBetweenColors);
+        }
+    }
+}
