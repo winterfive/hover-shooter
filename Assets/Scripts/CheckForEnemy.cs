8b01248e7498655377d56ba2ffd5e6528904bc10
winterfive <piealacode@gmail.com>
2018-09-27 12:46:58 -0400

    Listens for OnNewObjectFound and checks to see if its an enemy

---
+++ Assets/Scripts/CheckForEnemy.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+/*
+ * Checks player raycast hit for enemy
+ */
+public class CheckForEnemy : GenericManager<CheckForEnemy> {
+
+    public delegate void EnemySpotted();
+    public static event EnemySpotted OnEnemySpotted;
+
+    private RaycastManager _raycastManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _raycastManager = RaycastManager.Instance;		
+	}
+
+
+    /*
+     * Checks if current object seen by reticle is an enemy
+     * void -> void
+     */
+    public void CheckFoundForEnemy()
+    {
+        GameObject currentObject = _raycastManager.GetCurrentFoundObject();
+
+        if (currentObject.tag == "Enemy" || currentObject.tag == "Turret" || currentObject.tag == "Glow")
+        {
+            if (OnEnemySpotted != null)
+            {
+                OnEnemySpotted();
+            }
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        RaycastManager.OnNewObjectFound += CheckFoundForEnemy;
+    }
+
+
+    private void OnDisable()
+    {
+        RaycastManager.OnNewObjectFound -= CheckFoundForEnemy;
+    }
+
+
+}
