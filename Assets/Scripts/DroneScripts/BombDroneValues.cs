812a53aeaf7e7f82353af88379a69564c776cf7a
winterfive <piealacode@gmail.com>
2018-10-04 19:23:01 -0400

    Created script

---
+++ Assets/Scripts/DroneScripts/BombDroneValues.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombDroneValues : SetAsSingleton<BombDroneValues> {
+
+    /*
+     * This class holds values needed by the BombDrone class
+     */
+
+    public Transform[] spawnPoints;
+    public GameObject prefab;
+    public int poolSize;
+    public float xMin, xMax, yMin, yMax, zMin, zMax;
+    public float minSpeed, maxSpeed;
+    public float altitudeMin, altitudeMax;
+    public float waitToSpawn, timeBetweenSpawns;
+    public float glowSpeed;
+    public Color secondGlowColor;
+    public int damageValue, pointValue;    
+}
