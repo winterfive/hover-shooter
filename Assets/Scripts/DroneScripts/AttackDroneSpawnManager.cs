c81661246d02ffb20c9773d20f53e8e35502c1e5
winterfive <piealacode@gmail.com>
2018-10-04 16:39:38 -0400

    Revised comments, spacing

--- Assets/Scripts/DroneScripts/AttackDroneSpawnManager.cs
+++ Assets/Scripts/DroneScripts/AttackDroneSpawnManager.cs
@@ -14,6 +14,7 @@ public class AttackDroneSpawnManager : PoolingManager {
     private GameObject _activeAttackDrone;
     private DroneActions _droneActions;
 
+
     private void Awake()
     {
         _attackDrones = CreateList(_ADV.prefab, _ADV.poolSize);
@@ -27,7 +28,7 @@ public class AttackDroneSpawnManager : PoolingManager {
 
 
     /*
-     * Spawns attack drone
+     * Spawns attack drones at one of the spawnPoints
      * void -> void
      */
     private IEnumerator SpawnAttackDrone()
