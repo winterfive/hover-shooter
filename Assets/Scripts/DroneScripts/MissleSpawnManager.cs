7ad54db50b4df17c231250fa817367375e05d115
winterfive <piealacode@gmail.com>
2018-10-16 13:13:18 -0400

    Created script

---
+++ Assets/Scripts/DroneScripts/MissleSpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissleSpawnManager : PoolingManager {
+
+    private DroneMissleValues _DMV;
+    private List<GameObject> _missles;
+    private Sprite _activeMissle;
+    private MissleActions _missleActions;
+
+
+    private void Awake()
+    {
+        _DMV = DroneMissleValues.Instance;
+        _missles = CreateList(_DMV.prefab, _DMV.poolSize);
+    }
+
+
+
+}
