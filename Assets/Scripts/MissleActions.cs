c3c11dc17022b44c401225a90d84252505cc1414
winterfive <piealacode@gmail.com>
2018-10-16 13:16:07 -0400

    Created script

---
+++ Assets/Scripts/MissleActions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UnityEngine;
+
+public class MissleActions : MonoBehaviour
+{
+    public int missleSpeed;
+    public delegate void PlayerHit();
+    public static PlayerHit OnPlayerHit;
+    public delegate void ShieldHit();
+    public static ShieldHit OnShieldHit;
+
+    private GameObject _missle;
+    private Transform _camTransform;
+    private Transform _target;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        _camTransform = Camera.main.transform;
+        _missle = this.gameObject;
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (OnPlayerHit != null)
+            {
+                OnPlayerHit();
+            }
+        }
+
+        if (col.gameObject.tag == "Shield")
+        {
+            if (OnShieldHit != null)
+            {
+                OnShieldHit();
+            }
+        }
+
+        _missle.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (_missle.activeInHierarchy)
+        {
+            MissleFly();
+        }       
+    }
+
+
+    /*
+     * Checks if missle distance to see if it should continue flying
+     * void -> void
+     */
+    private void MissleFly()
+    {
+        if (Vector3.Distance(_missle.transform.position, _camTransform.position) > 0.5)
+        {
+            float step = missleSpeed * Time.deltaTime;
+            _missle.transform.position = Vector3.MoveTowards(_missle.transform.position, _camTransform.position, step);
+            _missle.transform.LookAt(_camTransform);
+        }
+    }
+}
