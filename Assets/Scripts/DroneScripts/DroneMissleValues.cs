9d2ce18be6a98793dd320350ee58ceef2fc73337
winterfive <piealacode@gmail.com>
2018-10-16 12:26:54 -0400

    Created script DroneMissleValues

---
+++ Assets/Scripts/DroneScripts/DroneMissleValues.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMissleValues {
+
+	public int minMSPeed, maxMSpeed;
+    public int minMFireRate, maxMFireRate;
+    
+}
