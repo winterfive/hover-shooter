14ceb6a25d6bc9ab07f1576e93b6444f5bf44ad8
winterfive <piealacode@gmail.com>
2018-10-16 13:15:33 -0400

    Created script

---
+++ Assets/Scripts/DroneScripts/MissleValues.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissleValues : SetAsSingleton<DroneMissleValues> {
+
+    public int poolSize;
+    public int minMSPeed, maxMSpeed;
+    public int minMFireRate, maxMFireRate;
+    public GameObject prefab;
+
+}
