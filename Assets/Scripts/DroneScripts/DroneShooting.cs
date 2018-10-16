ea404ba54000209a953b621b1ddce402f66cf767
winterfive <piealacode@gmail.com>
2018-10-16 13:36:09 -0400

    Added fields, script reference code, and ShootMissle()

--- Assets/Scripts/DroneScripts/DroneShooting.cs
+++ Assets/Scripts/DroneScripts/DroneShooting.cs
@@ -2,31 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DroneShooting : MonoBehaviour {
+public class DroneShooting : DroneActions {
 
-    private MissleValues _DMVRef;
-    private GameObject _gunTip;
+    private MissleValues _MVRef;
+    private GameObject _this;
+    private Transform _gunTipTransform;
+    private float _timeBetweenMissles;
+    private float _timeOfPreviousShot;
+    private RaycastHit _hit;
+    private int _droneRange;
 
     private void Awake()
     {
         GameObject missleValuesObject = GameObject.FindWithTag("ScriptManager");
         if (missleValuesObject != null)
         {
-            _DMVRef = missleValuesObject.GetComponent<MissleValues>();
+            _MVRef = missleValuesObject.GetComponent<MissleValues>();
         }
         else
         {
             Debug.Log("Cannot find DroneMissleValues script");
         }
+
+        _this = this.gameObject;
+        _gunTipTransform = FindChildWithTag("GunTip", _this);
+        _timeBetweenMissles = Random.Range(_MVRef.minMFireRate, _MVRef.maxMFireRate);
+        _droneRange = _MVRef.droneRange;
     }
 
-    
 
-    // Need call to shoot in update
+    private void Update()
+    {
+        ShootMissles();
+    }
+    
 
     private void ShootMissles()
     {
-        if (Time.time > _timeOfPreviousShot + _timeBetweenShots)
+        if (Time.time > _timeOfPreviousShot + _timeBetweenMissles)
         {
             if (Physics.Raycast(_gunTipTransform.position, -_gunTipTransform.forward, out _hit, droneRange))
             {
