5726136c23a3145dfb4a14fb2edd8ec1fc468756
winterfive <piealacode@gmail.com>
2018-10-16 12:27:30 -0400

    Created script for drone shooting actions

---
+++ Assets/Scripts/DroneScripts/DroneShooting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneShooting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void ShootMissles()
+    {
+        if (Time.time > _timeOfPreviousShot + _timeBetweenShots)
+        {
+            if (Physics.Raycast(_gunTipTransform.position, -_gunTipTransform.forward, out _hit, droneRange))
+            {
+                if (_hit.transform.tag == "Player")
+                {
+                    if (OnMissleFired != null)
+                    {
+                        OnMissleFired(_gunTipTransform);
+                        _timeOfPreviousShot = Time.time;
+                    }
+                }
+            }
+        }
+    }
+}
